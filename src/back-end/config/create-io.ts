d0f4c26ca686997c66b58c2ebe38ff7acf218781
Belkross <belkross.pro@gmail.com>
2023-02-13 03:07:24 +0100

    refactor: remove unused socket.io types

--- src/back-end/config/create-io.ts
+++ src/back-end/config/create-io.ts
@@ -1,9 +1,9 @@
 import { Server as SocketIoServer } from "socket.io"
 import type { Server } from "node:http"
-import { ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData } from "../../types/type-server"
+import { ClientToServerEvents, ServerToClientEvents } from "../../types/type-server"
 
 export default function createIo(httpServer: Server) {
-  return new SocketIoServer<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(httpServer, {
+  return new SocketIoServer<ClientToServerEvents, ServerToClientEvents>(httpServer, {
     cors: {
       origin: ["https://esquive.belkross.com", "http://localhost:5173", "http://localhost:4173"],
     },
--- src/types/type-server.ts
+++ src/types/type-server.ts
@@ -4,31 +4,15 @@ import { AlertId } from "../front-end/components/alert-feature/functions/alerts"
 
 export type ServerToClientEvents = {
   alertClient: (alertId: AlertId) => void
-  clientJoinedRoom: (data: {
-    sessionId: string
-    username: string
-    room: string
-    roomState: "roomState"
-  }) => void
+  clientJoinedRoom: (data: { sessionId: string; username: string; room: string; roomState: "roomState" }) => void
 }
 
 export type ClientToServerEvents = {
-  //...
+  //..
 }
 
-export interface InterServerEvents {
-  ping: () => void
-}
-
-export interface SocketData {
-  sessionId: string
-  userId: string
-  username: string
-  room: string
-}
-
-export type SocketManager = Socket<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>
-export type IoManager = Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>
+export type SocketManager = Socket<ClientToServerEvents, ServerToClientEvents>
+export type IoManager = Server<ClientToServerEvents, ServerToClientEvents>
 
 export type ServerManager = {
   io: IoManager
