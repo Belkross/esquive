bfef38d668da98356ff8cb27fbfb6ce1af4dd106
Belkross <belkross.pro@gmail.com>
2023-02-20 05:36:35 +0100

    refactor: remove code repetition and write self-documented code

--- src/back-end/config/room-state/methods/start-timer.ts
+++ src/back-end/config/room-state/methods/start-timer.ts
@@ -4,15 +4,19 @@ import { RoomState } from "../room-state.js"
 export async function startTimer(this: RoomState, io: Io) {
   this.timerIsRunning = true
 
-  await new Promise((resolve /* , reject */) => setTimeout(resolve, 1000)) //avoid the timer to instantly loose a second on activation
+  await oneSecond()
 
   while (this.timerIsRunning && this.timer > 0) {
     --this.timer
     io.in(this.roomName).emit("roomStateUpdate", this)
-    await new Promise((resolve) => setTimeout(resolve, 1000))
+    await oneSecond()
   }
   if (this.timer === 0) {
     this.configureNextRoundPhase()
     io.in(this.roomName).emit("roomStateUpdate", this)
   }
+}
+
+function oneSecond() {
+  return new Promise((resolve) => setTimeout(resolve, 1000))
 }
