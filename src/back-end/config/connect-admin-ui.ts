1ed28fe893060f816d96d8a2abcfd01075297ed9
Belkross <belkross.pro@gmail.com>
2023-03-14 14:08:07 +0100

    feature: implement socket.io admin ui authentication

--- src/back-end/config/connect-admin-ui.ts
+++ src/back-end/config/connect-admin-ui.ts
@@ -1,12 +1,13 @@
 import { Io } from "../../types/server.js"
 import { instrument } from "@socket.io/admin-ui"
+import bcrypt from "bcrypt"
 
 export function connectAdminUi(io: Io) {
   instrument(io, {
     auth: {
       type: "basic",
       username: "admin",
-      password: process.env.ADMIN_UI_PASSWORD || "admin",
+      password: bcrypt.hashSync(process.env.ADMIN_UI_PASSWORD || "admin", 10),
     },
   })
 }
--- src/back-end/config/create-io.ts
+++ src/back-end/config/create-io.ts
@@ -5,7 +5,8 @@ import { ClientToServerEvents, ServerToClientEvents } from "../../types/server"
 export function createIo(httpServer: Server) {
   return new SocketIoServer<ClientToServerEvents, ServerToClientEvents>(httpServer, {
     cors: {
-      origin: ["http://localhost:5173", "http://localhost:4173", `${process.env.CLIENT_URL}`],
+      origin: ["http://localhost:5173", "http://localhost:4173", `${process.env.CLIENT_URL}`, "https://admin.socket.io"],
+      credentials: true //needed by socket.io admin ui
     },
   })
 }
